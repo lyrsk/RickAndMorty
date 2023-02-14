e941655a4fdb44e83a59d7cd0cd51a0d3d2db4aa
Eliana <crisely.cye@gmail.com>
2023-02-14 01:25:25 -0300

    3

--- src/details.js
+++ src/details.js
@@ -1,6 +1,6 @@
 import {getCharacter} from "./services/getData.js";
 
-const container = document.querySelector('#characters');
+const container = document.querySelector('#character');
 const loader = document.querySelector('#lds-ring');
 const getID = localStorage.getItem('charID');
 
@@ -8,12 +8,19 @@ const loadCharacter = async (id) => {
     loader.style.display = 'grid';
     const data = await getCharacter(id);
     loader.style.display = 'none';
-
     const article = document.createElement('article');
-    article.setAttribute('class, character');
+    article.setAttribute('class', 'character');
     article.innerHTML = `
-    <img src='${data.image}'></img>
-    <h2>${data.name}</h2>
-    
-    `
-}
+        <img src="${data.image}" alt="">
+        <h2>${data.name}</h2>
+        <p class="data"> <span>Origen:</span> ${data.origin.name}</p>
+        <p class="data"> <span>Locación Actual:</span> ${data.location.name}</p>
+        <div>
+            <p class="data"> <span>Especie:</span> ${data.species}</p>
+            <p class="${data.status.toLowerCase()}"></p>
+        </div>
+    `;
+    container.appendChild(article);
+    };
+
+    loadCharacter(getID);
